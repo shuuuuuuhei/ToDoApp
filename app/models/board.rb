87b5b80f2ea79178a63fbad59b8b0e771b9fbdd2
shuhei <>
2020-10-31 16:47:24 +0900

    Boardモデルを作成

---
+++ app/models/board.rb
@@ -0,0 +1,12 @@
+# == Schema Information
+#
+# Table name: boards
+#
+#  id          :integer          not null, primary key
+#  description :text
+#  title       :string
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+class Board < ApplicationRecord
+end
