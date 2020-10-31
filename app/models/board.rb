0db8d7c22b9e63916e72c7e4651c960441d7a492
shuhei <>
2020-10-31 18:01:20 +0900

    UserモデルとBoardモデルの紐付け

--- app/models/board.rb
+++ app/models/board.rb
@@ -7,6 +7,12 @@
 #  title       :string
 #  created_at  :datetime         not null
 #  updated_at  :datetime         not null
+#  user_id     :integer
+#
+# Indexes
+#
+#  index_boards_on_user_id  (user_id)
 #
 class Board < ApplicationRecord
+    belongs_to :user
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_31_073511) do
+ActiveRecord::Schema.define(version: 2020_10_31_084452) do
 
   create_table "boards", force: :cascade do |t|
     t.string "title"
     t.text "description"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_boards_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
