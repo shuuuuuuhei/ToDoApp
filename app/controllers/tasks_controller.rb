47d2d42a8439e37244261cb3cdc7ee0db449ede2
shuhei <>
2020-11-20 06:58:10 +0900

    destroyアクションを追加

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -19,6 +19,16 @@ class TasksController < ApplicationController
         @task = Task.find(params[:id])
     end
 
+    def destroy
+        board = Board.find(params[:board_id])
+        @task = Task.find(params[:id])
+        if @task.destroy!
+            redirect_to board_path(board), notice: 'タスクを削除しました'
+        else
+            flash.now[:error] = '削除に失敗しました'
+        end
+    end
+
     private
     def task_params
         params.require(:task).permit(:title, :content, :limit).merge(user_id: current_user.id)
