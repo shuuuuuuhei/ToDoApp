c61d8faed08e332d747d8e6255d3281fc9cfa376
shuhei <>
2020-11-21 15:54:32 +0900

    taskカラムのバリデーションを追加

--- app/models/task.rb
+++ app/models/task.rb
@@ -22,7 +22,6 @@ class Task < ApplicationRecord
     belongs_to :user
     belongs_to :board
 
-    validates :title, presence: true
     validates :content, presence: true
     validates :limit, presence: true
 end
