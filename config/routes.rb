8a68b8767f4fd84fb8593b727cabe907422399d2
shuhei <>
2020-11-22 08:23:37 +0900

    Commentのルーティングを設定

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'boards#index'
   resources :boards do
-    resources :tasks
+    resources :tasks do
+      resources :comments
+    end
   end
     
 end
