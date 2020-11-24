18b0e6cb98c1fdb6b944cb190221c5b96434d192
shuhei <>
2020-11-24 16:51:30 +0900

    プロフィールを編集・更新機能を追加

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -5,7 +5,27 @@ class ProfilesController < ApplicationController
     end
 
     def edit
-
+        @profile = current_user.prepare_profile
     end
 
+    def update
+        @profile = current_user.prepare_profile
+        @profile.assign_attributes(profile_params)
+        if @profile.save!
+            redirect_to profile_path, notice: '更新できました'
+        else
+            flash.now[:error] = '更新できませんでした'
+            render 'edit'
+        end
+    end
+
+    private
+    def profile_params
+        params.require(:profile).permit(
+            :nickname,
+            :introduction,
+            :gender,
+            :subscribed
+        )
+    end
 end
