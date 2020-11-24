b1c63de2d40e0445149dc74a0df8baa54526e2a6
shuhei <>
2020-11-24 16:51:34 +0900

    delegateを用いてuserのプロフィール画面を表示

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -17,4 +17,6 @@
 #
 class Profile < ApplicationRecord
     belongs_to :user
+
+    enum gender: { male: 0, female: 1, other: 2 }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -27,4 +27,15 @@ class User < ApplicationRecord
   has_many :comments, dependent: :destroy
 
   has_one :profile, dependent: :destroy
+
+  delegate :introduction, :gender, to: :profile, allow_nil: 'true'
+
+  def display_name
+    #ぼっち演算子
+    profile&.nickname || self.email.split('@').first
+  end
+
+  def prepare_profile
+    profile || build_profile
+  end
 end
